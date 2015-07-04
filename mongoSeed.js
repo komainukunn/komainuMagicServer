953157210c9017b15deb43623d2f697d138b928d
koma <msy.register@gmail.com>
2015-07-04 20:37:37 +0900

    アカウントのシードデータ追加

--- mongoSeed.js
+++ mongoSeed.js
@@ -1,49 +1,21 @@
 var mongoose = require("mongoose");
-
-//テーブル作成
-var Schema   = mongoose.Schema;
-
-var ArticleTable = new Schema({
-  title:{type: String, required: true},
-  text:{type: String, required: true},
-  data:{type: Date, default: Date.now}
-});
-
-var CategoryTable = new Schema({
-    categoryName:{type: String, required: true}
-});
-
-var ArticleCategoryTable = new Schema({
-    article_id:{type: Object, required:true},
-    category_id:{type: Object, required:true}
-});
-
-mongoose.model("Articles",ArticleTable);
-mongoose.model("Categories",CategoryTable);
-mongoose.model("ArticlesCategories",ArticleCategoryTable);
-
-//接続
-mongoose.connect("mongodb://localhost/komainukunndb");
+var db = require("./model/database");
 
 var Article = mongoose.model("Articles");
 var Category = mongoose.model("Categories");
 var Article_Category = mongoose.model("ArticlesCategories");
+var Account = mongoose.model("Accounts");
 
 //シード
 var article = new Article();
-article.title  = "Mongo Test";
-article.text = "ドキドキのテスト"; 
+article.title  = "はじめまして";
+article.text = "test"; 
 article.save(function(err) {
       if (err) { console.log(err); }
 });
 
 var category = new Category();
-category.categoryName = "カテゴリー1";
-category.save(function(err) {
-      if (err) { console.log(err); }
-});
-
-category.categoryName = "カテゴリー2";
+category.categoryName = "マジック";
 category.save(function(err) {
       if (err) { console.log(err); }
 });
@@ -55,4 +27,24 @@ article_category.save(function(err) {
           if (err) { console.log(err); }
 });
 
-console.log("end");
+var category = new Category();
+category.categoryName = "雑記";
+category.save(function(err) {
+      if (err) { console.log(err); }
+});
+
+var article_category = new Article_Category();
+article_category.article_id  = article._id;
+article_category.category_id = category._id;
+article_category.save(function(err) {
+          if (err) { console.log(err); }
+});
+
+var account = new Account();
+account.id = "koomainu";
+account.password = "koomainukunn";
+account.save(function(err) {
+          if (err) { console.log(err); }
+});
+
+console.log("errがでなければ完了ですよ");
